4004d2a6bc3f48dc1c4f73edffd8d8b3c0f25fdb
gudo1234 <izumilitee@gmail.com>
2026-01-09 00:40:10 -0600

    Delete plugins directory

--- plugins/events/response_bot.js
+++
@@ -1,7 +0,0 @@
-let handler = m => m
-
-handler.before = async (m) => {
-  if (m.text.endsWith("bot") || m.text.endsWith("Bot")  || m.text.endsWith("corazon") || m.text.endsWith("Corazon")) return m.reply("_Hmm, Doishtano? senpai!_")
-}
-
-export default handler;
