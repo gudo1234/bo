0a3cceacd8d97a1a6fbc9f3347531f7283aeb4c5
gudo1234 <izumilitee@gmail.com>
2026-01-09 01:53:56 -0600

    Remove unused getRandomChannel function
    Removed commented-out async function for random channel selection.

--- config.js
+++ config.js
@@ -15,16 +15,6 @@ global.owner = [
 global.mods = []
 global.prems = []
 //❤️❤️❤️❤️
-/*async function getRandomChannel() {
-  const canalIdM = ["120363285614743024@newsletter", "120363395205399025@newsletter"]
-  const canalNombreM = ["🤖⃧►iʑυвöτ◃2.0▹", "Zeus Bot🔆Channel-OFC"]
-
-  const idx = Math.floor(Math.random() * canalIdM.length)
-  return {
-    id: canalIdM[idx],
-    name: canalNombreM[idx]
-  }
-}*/
 const emojiList = [
     "🌱", "🪴", "⭐", "🍁", "⚡",
     "🌙", "🏖️", "🪐", "✨", "🌊"
@@ -85,15 +75,14 @@ global.a1 = `╭┈۫۫۫۫۫╌۪۪۪۪۪۪۪۪֠╼◈¨(`
 global.a2 = `)¨◈۫۫۫۫۫۫۫۫۫╾֩┈۪۪۪۪╮`
 //✅✅✅✅
 
-
 global.packname = '銉熲槄 饾槍饾槸饾槳饾槰饾槷饾槩-饾槈饾槹饾樀 鈽呭健'
 global.textbot = "Bot de mierd4";
 global.author = "🍁̶͢͞▻⟅ẹ𝆊ϋ⟆٭⳺֟፝͜⳻٭.er/k";
+global.wm = "🤖⃧►iʑυвöτ◃2.0▹"
 global.wait = 'Espera Por Favot'
 global.botname = '銉熲槄 饾槍饾槸饾槳饾槰饾槷饾槩-饾槈饾槹饾樀 鈽呭健'
 global.listo = 'Se completo tarea'
 global.namechannel = '銉熲槄 饾槍饾槸饾槳饾槰饾槷饾槩-饾槈饾槹饾樀 鈽呭健'
-
 
 global.group = 'https://chat.whatsapp.com/Ioz'
 global.canal = 'https://whatsapp.com/channel/0029'
@@ -103,12 +92,8 @@ global.fs = fs
 global.fetch = fetch
 global.axios = axios
 
-
-
 global.multiplier = 69 
 global.maxwarn = '2' // m谩xima advertencias
-
-
 
 let file = fileURLToPath(import.meta.url)
 watchFile(file, () => {
