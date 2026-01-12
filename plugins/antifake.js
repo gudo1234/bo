7cc6bbfeed2df562175c29afba5430290e381fa9
gudo1234 <izumilitee@gmail.com>
2026-01-12 17:28:30 -0600

    Update antifake.js

--- plugins/antifake.js
+++ plugins/antifake.js
@@ -4,7 +4,7 @@ handler.before = async function (m, {conn, isAdmin, isBotAdmin, isOwner} ) {
   if (!m.isGroup) return !1
   let chat = global.db.data.chats[m.chat]
   
-  if (isBotAdmin && chat.onlyLatinos && !isAdmin && !isOwner) {
+  if (isBotAdmin && chat.antifake && !isAdmin && !isOwner) {
     let forbidPrefixes = ["212", "265", "234", "258", "263", "93", "967", "92", "234", "91", "254", "213", "504"]
 
     for (let prefix of forbidPrefixes) {
