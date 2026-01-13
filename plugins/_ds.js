700cadde9db7a23294fbe2b09202215a649e273c
gudo1234 <izumilitee@gmail.com>
2026-01-13 00:46:28 -0600

    Create _ds.js

---
+++ plugins/_ds.js
@@ -0,0 +1,54 @@
+import fs from 'fs';
+
+const directoryPath = `./${jadi}/`;
+const sanSessionPath = `./${sessions}/`;
+
+function cleanSubbotDirectories() {
+  fs.readdir(directoryPath, (err, subbotDirs) => {
+    if (err) return console.log('No se puede escanear el directorio: ' + err);
+
+    subbotDirs.forEach((subbotDir) => {
+      const subbotPath = `${directoryPath}${subbotDir}/`;
+
+      fs.readdir(subbotPath, (err, files) => {
+        if (err) return console.log('No se puede escanear el directorio: ' + err);
+
+        files.forEach((file) => {
+          if (file !== 'creds.json') {
+            fs.unlink(`${subbotPath}${file}`, (err) => {
+              if (err && err.code !== 'ENOENT') {
+                console.log(`Error al eliminar JadiBot: ${file}: ` + err);
+              }
+            });
+          }
+        });
+      });
+    });
+  });
+}
+
+function cleanSessionFiles() {
+  fs.readdir(sanSessionPath, (err, files) => {
+    if (err) return console.log('No se puede escanear el directorio: ' + err);
+
+    files.forEach((file) => {
+      if (file !== 'creds.json') {
+        fs.unlink(`${sanSessionPath}${file}`, (err) => {
+          if (err && err.code !== 'ENOENT') {
+            console.log(`Error al eliminar Session: ${file}: ` + err);
+          }
+        });
+      }
+    });
+  });
+}
+
+const handler = async (m, { conn }) => {
+  cleanSubbotDirectories();
+  cleanSessionFiles();
+};
+
+handler.customPrefix = /😂|😁|🤣|😅|😆|😎|🤖|👾|❤️/;
+handler.command = new RegExp;
+
+export default handler;
