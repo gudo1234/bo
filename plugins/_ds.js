835d53aab0e5dc5aff787dc87c06b33c76c18e8a
gudo1234 <izumilitee@gmail.com>
2026-01-13 00:53:49 -0600

    Update _ds.js

--- plugins/_ds.js
+++ plugins/_ds.js
@@ -1,21 +1,25 @@
 import fs from 'fs';
+import { join } from 'path';
 
-const directoryPath = `./${jadi}/`;
-const sanSessionPath = `./${sessions}/`;
+const jadi = 'JadiBots';
+const sessions = 'sessions';
+
+const directoryPath = join('.', jadi);
+const sanSessionPath = join('.', sessions);
 
 function cleanSubbotDirectories() {
   fs.readdir(directoryPath, (err, subbotDirs) => {
     if (err) return console.log('No se puede escanear el directorio: ' + err);
 
     subbotDirs.forEach((subbotDir) => {
-      const subbotPath = `${directoryPath}${subbotDir}/`;
+      const subbotPath = join(directoryPath, subbotDir);
 
       fs.readdir(subbotPath, (err, files) => {
         if (err) return console.log('No se puede escanear el directorio: ' + err);
 
         files.forEach((file) => {
           if (file !== 'creds.json') {
-            fs.unlink(`${subbotPath}${file}`, (err) => {
+            fs.unlink(join(subbotPath, file), (err) => {
               if (err && err.code !== 'ENOENT') {
                 console.log(`Error al eliminar JadiBot: ${file}: ` + err);
               }
@@ -33,7 +37,7 @@ function cleanSessionFiles() {
 
     files.forEach((file) => {
       if (file !== 'creds.json') {
-        fs.unlink(`${sanSessionPath}${file}`, (err) => {
+        fs.unlink(join(sanSessionPath, file), (err) => {
           if (err && err.code !== 'ENOENT') {
             console.log(`Error al eliminar Session: ${file}: ` + err);
           }
@@ -49,6 +53,6 @@ const handler = async (m, { conn }) => {
 };
 
 handler.customPrefix = /😂|😁|🤣|😅|😆|😎|🤖|👾|❤️/;
-handler.command = new RegExp;
+handler.command = new RegExp(); // así funciona correctamente
 
 export default handler;
