6d77cdc3d4e001c3f41f61551d0f1a3cf8f28059
gudo1234 <izumilitee@gmail.com>
2026-01-12 19:19:34 -0600

    Update _salir.js

--- plugins/_salir.js
+++ plugins/_salir.js
@@ -5,21 +5,34 @@ let handler = async (m, { conn, text, command }) => {
   let id = text ? text : m.chat  
   let groupMetadata = await conn.groupMetadata(id)
 
-  let fechaHoraMX = moment().tz('America/Mexico_City').locale('es').format('dddd D [de] MMMM [del] YYYY [a las] h:mm a [hora México]')
+  let fechaHoraMX = moment()
+    .tz('America/Mexico_City')
+    .locale('es')
+    .format('dddd D [de] MMMM [del] YYYY [a las] h:mm a [hora México]')
 
- let txt = `${e} \`Saliendo automáticamente del grupo...\`\n*Nombre:* ${groupMetadata.subject}\n*ID:* ${id}\n> ${fechaHoraMX}`
-  await conn.sendButton2(m.chat, txt, textbot, icono, [], null, [['ᴏᴡɴᴇʀ/true', `https://wa.me/50492280729?text=Hola+quiero+un+bot+para+mi+grupo,+cuáles+son+los+planes?+`], ['sᴇɢᴜɪʀ ᴄᴀɴᴀʟ', canal]], m)
+  let txt = `${e} \`Saliendo automáticamente del grupo...\`\n*Nombre:* ${groupMetadata.subject}\n*ID:* ${id}\n> ${fechaHoraMX}`
+
+  await conn.sendButton2(
+    m.chat,
+    txt,
+    textbot,
+    icono,
+    [],
+    null,
+    [
+      ['ᴏᴡɴᴇʀ/true', `https://wa.me/50492280729?text=Hola+quiero+un+bot+para+mi+grupo,+cuáles+son+los+planes?+`],
+      ['sᴇɢᴜɪʀ ᴄᴀɴᴀʟ', canal]
+    ],
+    m
+  )
 
   await new Promise(resolve => setTimeout(resolve, 3000))
 
-  //await conn.groupLeave(id)
+  /** await conn.groupLeave(id) **/
 }
-
 
 handler.customPrefix = /🚲/
 handler.command = new RegExp
-//handler.command = ['salir']
 handler.group = true
-//handler.rowner = true
 
 export default handler
