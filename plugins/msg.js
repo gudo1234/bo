20fe1961b7af742a380c7b52dbade571ae0618c9
gudo1234 <izumilitee@gmail.com>
2026-01-20 17:54:20 -0600

    Update msg.js

--- plugins/msg.js
+++ plugins/msg.js
@@ -3,7 +3,7 @@ if (!text) throw `🌱 Ingresa el texto que se enviará junto a los botones.`
 let txxt = "hola, follamos?"
 let txxt2 = "quiero de tu lechita🫦"
 
-await conn.sendButton('120363285614743024@newsletter', text, "holi :)", icono, [], null, [['tocar🫦', `https://wa.me/5493425242334?text=${txxt}`], ['mi Instagram bb', `https://www.instagram.com/edar504__?`]], m)
+await conn.sendButton2('120363285614743024@newsletter', text, "holi :)", icono, [], null, [['tocar🫦', `https://wa.me/5493425242334?text=${txxt}`], ['mi Instagram bb', `https://www.instagram.com/edar504__?`]], m)
  
 await m.reply('Se envío con éxito el texto al canal!')
 }
