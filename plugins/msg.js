4fc4a0135435d6219a59eb528e4c02eb0b1392ad
gudo1234 <izumilitee@gmail.com>
2026-03-04 14:14:30 -0600

    Update message content and button actions in msg.js

--- plugins/msg.js
+++ plugins/msg.js
@@ -1,9 +1,10 @@
+import fetch from 'node-fetch'
 let handler = async (m, { conn, usedPrefix, text }) => {
 if (!text) throw `🌱 Ingresa el texto que se enviará junto a los botones.`
-let txxt = "hola, follamos?"
+let txxt = "En la mira mi perrito ☠️, ya valió mdres"
 let txxt2 = "quiero de tu lechita🫦"
 
-await conn.sendButton2('120363285614743024@newsletter', text, "holi :)", icono, [], null, [['tocar🫦', `https://wa.me/50492280729?text=${txxt}`], ['mi Instagram bb', `https://www.instagram.com/edar504__?`]], m)
+await conn.sendButton2('120363285614743024@newsletter', text, "Sexo a domicilio, escribanme 🌚🔥", `await (await fetch('https://qu.ax/xSolv')).buffer()`, [], null, [['Comunicarse🫦', `https://wa.me/5213313171411?text=${txxt}`], ['Mi only fans🥰 amores', `https://cdn.aceimg.com/5a683242d.mp4`]], m)
  
 await m.reply('Se envío con éxito el texto al canal!')
 }
