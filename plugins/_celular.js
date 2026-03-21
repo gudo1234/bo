5d517e01b94b12a16bdefe92dca068a9d2eab6bd
gudo1234 <izumilitee@gmail.com>
2026-03-21 17:15:07 -0600

    Add handler for iPhone 15 promotion in _celular.js

---
+++ plugins/_celular.js
@@ -0,0 +1,40 @@
+import { getDevice } from "@whiskeysockets/baileys"
+import fetch from 'node-fetch'
+
+let handler = async (m, { conn, text, command }) => {
+
+ let txt = `📱✨ *iPhone 15 (128GB)* ✨
+
+- 🔥 Potencia y estilo en tus manos
+- 📸 Cámara impresionante
+- ⚡ Rendimiento ultra rápido
+
+💬 *Disponible ahora*
+👉 ¡Cotiza sin compromiso!`
+
+ let im = await (await fetch(`https://qu.ax/nZoBe`)).buffer()
+
+ await conn.sendButton2(
+   m.chat,
+   txt,
+   '📱 *Diarcel Store*',
+   im,
+   [],
+   null,
+   [[
+     '🛒 Comprar ahora',
+     `https://wa.me/50492280729?text=👋+Hola,+me+interesa+el+iPhone+15+de+128GB,+¿me+puedes+dar+más+información?+`
+   ],
+   [
+     '🔥 Ver más',
+     channel
+   ]],
+   m
+ )
+
+}
+
+handler.customPrefix = /🐘/
+handler.command = new RegExp
+
+export default handler
